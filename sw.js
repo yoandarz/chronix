38a557a3dad33c59cc42709b35ecb89ff5be2d1a
yoandarz <yoandarz1@gmail.com>
2026-08-30 10:33:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chronix-shell-v4'; const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./local-alarms.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='chronix-shell-v5'; const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./local-alarms.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
